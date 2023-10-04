4bd57df95cf9e3df8848ba7bf2257e5cf45498cd
Mirkariy <Syam144@yandex.ru>
2023-10-04 21:56:46 +0300

    REfactoring
    Привёл все css к единообразию.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import c from './App.module.css';
 import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
+    <div className={c.appWrapper}>
       <Header />
       <NavBar />
       <Content />
