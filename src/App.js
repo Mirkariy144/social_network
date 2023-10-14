3a3034fd92d73c9431222cc7268813dc9ef133d9
Mirkariy144 <Syam144@yandex.ru>
2023-10-14 16:29:16 +0300

    kurwa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 
+<<<<<<< Updated upstream
 const App = () => {
+=======
+const App = (props) => {
+>>>>>>> Stashed changes
   return (
     <div className={c.appWrapper}>
       <Header />
---
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import c from "./Messages.module.css"
+
+const Messages = (props) => {
+
+  let RenderMessage = props.Message.map( Message => <div>{Message.text}</div> );
+
+  let messageText = React.createRef();
+
+  let sendMessage = () => {
+      // let message = messageText.current.value;
+      props.newMessage();
+      props.updateMessageText('KURWA');
+  };
+
+  let newMessageChange = () => {
+    let message = messageText.current.value;
+    props.updateMessageText(message);
+  };
+
+  return (
+    <div className={c.MessageText}>
+      <div>  
+        {RenderMessage}
+      </div>
+      <div>
+        <textarea ref={messageText} onChange={newMessageChange} value={props.updateMessageInput} />
+        <button onClick={sendMessage}>Send message</button>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
