1220daf395f1ef52af24f6f8771d42f8430b16f6
Mirkariy <Syam144@yandex.ru>
2023-10-16 21:40:28 +0300

    Old Methods вынесены в другой файл

---
+++ src/OldMethods.js
@@ -0,0 +1,30 @@
+  // newPost() {
+  //   let newPostPush = {
+  //     text: this._State.ShitPosts.newPostLetter
+  //   };
+  
+  //   this._State.ShitPosts.Post.push(newPostPush)
+  //   this._State.ShitPosts.newPostLetter = ('');
+  //   this._callSubscriber();
+  // },
+
+  // newPostWrighting(letter) {
+  //   this._State.ShitPosts.newPostLetter = letter;
+  //   this._callSubscriber();
+  // },
+
+  // newMessage() {
+  //   let newMessagePush = {
+  //     text: this._State.Messages.updateMessageInput,
+  //   };
+  
+  //   this._State.Messages.Message.push(newMessagePush)
+  //   this._State.Messages.updateMessageInput = ('');
+  //   this._callSubscriber();
+  // },
+
+  // updateMessageText(letter) {
+  //   this._State.Messages.updateMessageInput = letter;
+  //   this._callSubscriber();
+  // },
+  
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -67,36 +67,6 @@ let store = {
     }
   },
 
-  // newPost() {
-  //   let newPostPush = {
-  //     text: this._State.ShitPosts.newPostLetter
-  //   };
-  
-  //   this._State.ShitPosts.Post.push(newPostPush)
-  //   this._State.ShitPosts.newPostLetter = ('');
-  //   this._callSubscriber();
-  // },
-
-  // newPostWrighting(letter) {
-  //   this._State.ShitPosts.newPostLetter = letter;
-  //   this._callSubscriber();
-  // },
-
-  // newMessage() {
-  //   let newMessagePush = {
-  //     text: this._State.Messages.updateMessageInput,
-  //   };
-  
-  //   this._State.Messages.Message.push(newMessagePush)
-  //   this._State.Messages.updateMessageInput = ('');
-  //   this._callSubscriber();
-  // },
-
-  // updateMessageText(letter) {
-  //   this._State.Messages.updateMessageInput = letter;
-  //   this._callSubscriber();
-  // },
-  
   subscriber(observer) {
     this._callSubscriber = observer
   },
