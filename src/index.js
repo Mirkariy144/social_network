31c5a8893b6d5ac44bc34119f9144a60a26b850e
Mirkariy144 <Syam144@yandex.ru>
2023-12-24 12:22:06 +0300

    Enchance

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { AuthProvider } from './React/CostomHooks/CastomHooks';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <AuthProvider>
         <App />
