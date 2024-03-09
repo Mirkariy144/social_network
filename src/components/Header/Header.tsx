799df04a379307b0ea939b27e4e153209ce99cdd
Mirkariy144 <Syam144@yandex.ru>
2024-03-09 17:11:32 +0300

    MaterialUI
    Начал перерабатывать соцсеть на Material UI. Переделаны NavBar и Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import c from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { AccountBox } from '@mui/icons-material';
 
 interface User {
   user?: {
@@ -11,21 +24,90 @@ interface User {
 }
 
 const Header: React.FC<User> = ({ user, logout }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null,
+  );
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className={c.appHeader}>
-      <img src="https://avatars.mds.yandex.net/i?id=9e935d9a791083dc250d6a14801ab3aac7276b15-10521504-images-thumbs&n=13" />
-      {user ? (
-        <div className={c.login}>
-          <div>{user.login}</div>
-          <div>{user.email}</div>
-          <button onClick={logout}>Logout</button>
-        </div>
-      ) : (
-        <div className={c.login}>
-          <NavLink to="/login">Login</NavLink>
-        </div>
-      )}
-    </div>
+    <AppBar position="static" sx={{ gridArea: 'a' }}>
+      <Container maxWidth="xl">
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <AccountBox sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Вwarthunder
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}></Box>
+          <AccountBox sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Манипуляции">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem key={'Выход'} onClick={logout}>
+                <Typography textAlign="center">{'Выход'}</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import c from './navBar.module.css';
+import {
+  Box,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import { AccountBox, Forum, People } from '@mui/icons-material';
 
 const NavBar = ({ id }: { id: number }) => {
   return (
-    <div className={c.appNavBar}>
-      <div className={c.item}>
-        <NavLink to={`profile/${id}`}>Профиль</NavLink>
-      </div>
-      <div className={c.item}>
-        <NavLink to={'messages'}>Сообщения</NavLink>
-      </div>
-      <div className={c.item}>
-        <NavLink to={'users'}>Пользователи</NavLink>
-      </div>
-    </div>
+    <Box sx={{ gridArea: 'b' }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          ['& .MuiDrawer-paper']: {
+            width: 240,
+            boxSizing: 'border-box',
+            top: 'auto',
+            left: 'auto',
+          },
+        }}
+      >
+        <Box sx={{ overflow: 'auto', margin: '10px' }}>
+          <List sx={{ gridArea: 'b' }}>
+            {[
+              { name: 'Профиль', link: `/profile/${id}`, icon: <AccountBox /> },
+              { name: 'Сообщения', link: '/messages', icon: <Forum /> },
+              { name: 'Пользователи', link: '/users', icon: <People /> },
+            ].map(({ name, link, icon }) => (
+              <NavLink key={name} to={link} style={{ textDecoration: 'none' }}>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{icon}</ListItemIcon>
+                    <ListItemText primary={name} />
+                  </ListItemButton>
+                </ListItem>
+              </NavLink>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
   );
 };
 
